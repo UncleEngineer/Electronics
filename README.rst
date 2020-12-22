d055c87b254c6e3343e993675084c489e40a7fba
Uncle Engineer <loong.wissawakorn@gmail.com>
2020-12-22 15:30:42 +0700

    Add files via upload

---
+++ README.rst
@@ -0,0 +1,29 @@
+โปรแกรมช่วยคำนวณด้าน electronics
+================================
+
+วิธีติดตั้ง (installation)
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+เปิด CMD / Terminal
+
+.. code:: python
+
+    pip install electronics
+
+วิธีใช้
+~~~~~~~
+
+เปิด IDLE ขึ้นมาแล้วพิมพ์...
+
+.. code:: python
+
+    import electronics
+
+เปิด cmd / terminal ขึ้นมาแล้วพิมพ์...
+
+.. code:: python
+
+    python -m electronics
+
+พัฒนาโดย: ลุงวิศวกร สอนคำนวณ FB: https://www.facebook.com/UncleEngineer
+YouTube: https://www.youtube.com/UncleEngineer
---
+++ setup.py
@@ -0,0 +1,37 @@
+﻿import io
+from os.path import abspath, dirname, join
+from setuptools import find_packages, setup
+
+
+HERE = dirname(abspath(__file__))
+LOAD_TEXT = lambda name: io.open(join(HERE, name), encoding='UTF-8').read()
+DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
+    'README.rst'
+])
+
+setup(
+  name = 'electronics',         
+  packages = ['electronics'],  
+  version = '0.0.1',     
+  license='MIT',       
+  description = 'Tools for Electronics by Uncle Engineer',   
+  long_description=DESCRIPTION,
+  author = 'Uncle Engineer',                   
+  author_email = 'loong.wissawakorn@gmail.com',     
+  url = 'https://github.com/UncleEngineer/electronics',
+  download_url = 'https://github.com/UncleEngineer/electronics/archive/v_001.tar.gz',    
+  keywords = ['Electronics', 'Uncle Engineer'],  
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
